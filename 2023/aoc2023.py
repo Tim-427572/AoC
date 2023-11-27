9deafae14cd733d132c3ea22f6efa4dfe669e9d8
Tim Burbach <timothy.j.burbach@intel.com>
2023-11-27 09:58:09 -0600

    AoC 2023 preparation work.

---
+++ 2023/aoc2023.py
@@ -0,0 +1,139 @@
+import re
+import sys
+import math
+import time
+import copy
+import curses # pip install windows-curses
+import pickle
+import socket
+import string
+import requests
+import functools
+import itertools
+import statistics
+import collections
+import numpy as np
+from os import path
+
+
+# Advent of Code
+# Never did spend the time to work out how to get oAuth to work so this code expects you to
+# manually copy over your session cookie value.
+# Using a web browser inspect the cookies when logged into the Advent of Code website.
+# Copy the value from the "session" cookie into a text file called "session.txt"
+
+# Constants
+_code_path = r'c:\AoC'
+_offline = False
+_year = 2023
+
+
+def _check_internet(host="8.8.8.8", port=53, timeout=2):
+    """
+    Attempt to check for the firewall by connecting to Google's DNS.
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    # except socket.error as ex:
+        # print(ex)
+    except socket.error:
+        return False
+
+
+def _pull_puzzle_input(day, seperator, cast):
+    """
+    Pull the puzzle data from the AOC website.
+
+    :param day: (int,str) the AoC day puzzle input to fetch or an example puzzle string
+    :param seperator: (str,None) A string separator to pass into str.split when consuming the puzzle data.
+        If None or "" don't try and split the puzzle input.
+    :param cast: (None,type) A Python function often a type cast (int, str, lambda) to be run against each data element.
+
+    :return: tuple of the data.
+    """
+    global _work, _offline, _code_path
+
+    if _offline:
+        with open(_code_path + r"\{}\day{}.txt".format(_year, day)) as file_handler:
+            data_list = file_handler.read().split(seperator)
+    elif isinstance(day, str):  # An example string
+        data_list = day.split(seperator)
+    else:
+        if not path.exists(_code_path + "/session.txt"):
+            raise Exception("Using the web browser get the session cookie value\nand put it as a string in {}".format(_code_path + "\session.txt"))  # noqa: W605
+        with open(_code_path + "/session.txt", 'r') as session_file:
+            session = session_file.read()
+        # Check to see if behind the firewall.
+        if _check_internet():
+            proxy_dict = {}
+        else:
+            proxy_dict = {'http': 'proxy-dmz.intel.com:911',
+                          'https': 'proxy-dmz.intel.com:912'}
+        header = {'Cookie': 'session={:s}'.format(session.rstrip('\n'))}
+        with requests.Session() as session:
+            resp = session.get('https://adventofcode.com/{}/day/{}/input'.format(_year, day), headers = header, proxies = proxy_dict)  # noqa: E251
+            _ = resp.text.strip("\n")
+            if resp.ok:
+                if seperator in [None, ""]:
+                    data_list = [resp.text]
+                else:
+                    data_list = resp.text.split(seperator)
+            else:
+                print("Warning website error")
+                return ()
+
+    if data_list[-1] == "":
+        data_list.pop(-1)
+    if cast is not None:
+        data_list = [cast(x) for x in data_list]
+    return tuple(data_list)
+
+
+# Cache the data in a pickle file.
+def get_input(day, seperator, cast, override=False):
+    """
+    Helper function for the daily puzzle information.
+    If the puzzle data does not exist it attempts to pull it from the website.
+    Caches the puzzle data into a pickle file so that re-runs don't have the performance
+    penalty of fetching from the Advent Of Code website.
+    :param day: (int, str) the AoC day puzzle input to fetch or a string of the puzzle example.
+    :param seperator: (str) A string separator to pass into str.split when consuming the puzzle data.
+    :param cast: (None,type) A Python function often a type cast (int, str, lambda) to be run against each data element.
+                             None - do not apply a function/cast to the data.
+    :param override: (bool) True = Fetch the data again instead of using the cached copy.
+
+    :return: tuple containing the puzzle data
+    """
+    global _code_path
+    if path.exists(_code_path + r'\{}\input.p'.format(_year)):
+        puzzle_dict = pickle.load(open(_code_path + r'\{}\input.p'.format(_year), 'rb'))
+    else:  # No pickle file, will need to make a new one.
+        puzzle_dict = {}
+
+    puzzle_input = puzzle_dict.get(day, None)
+
+    if puzzle_input is None or override is True:
+        puzzle_input = _pull_puzzle_input(day, seperator, cast)
+        if isinstance(day, int):  # only save the full puzzle data to the pickle file.
+            puzzle_dict[day] = puzzle_input
+            pickle.dump(puzzle_dict, open(_code_path + r'\{}\input.p'.format(_year), 'wb'))
+    return puzzle_input
+
+
+def day1():
+    """
+    So it begins!
+    """
+    day = ""
+    day = 1
+    puzzle = get_input(day, '\n', None)
+
+def go(day=1):
+
+    try:
+        return eval("day{}".format(day))
+    except Exception as e:
+        print(e)
+
