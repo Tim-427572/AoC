ed0597c62554719ae4083df374a7d1c8d8dbb2f0
Tim Burbach <timothy.j.burbach@intel.com>
2023-12-03 23:30:03 -0600

    Initial solution 2023, day 4

--- 2023/aoc2023.py
+++ 2023/aoc2023.py
@@ -1,4 +1,4 @@
-# import re
+import re
 # import sys
 # import math
 # import time
@@ -7,6 +7,7 @@
 import pickle
 import socket
 import pyglet
+import hashlib
 # import string
 import requests
 # import functools
@@ -372,6 +373,73 @@ def day3_p2(example=False, reload=False):
     print(gear_ratio_sum)
     #return patches
 
+
+def day3(example=False, reload=False):
+    day = 3 if not example else ("467..114..\n"
+                                 "...*......\n"
+                                 "..35..633.\n"
+                                 "......#...\n"
+                                 "617*......\n"
+                                 ".....+.58.\n"
+                                 "..592.....\n"
+                                 "......755.\n"
+                                 "...$.*....\n"
+                                 ".664.598..\n")
+    schematic = get_input(day, "\n", None)
+    schematic_array = np.array(list(map(list, schematic)))
+    schematic_array = np.pad(schematic_array, 1, 'constant', constant_values = ".")
+    number_positions = {}
+    number_hashes = {}
+    num_regex = re.compile("[0-9]\w+")
+    for row_idx, row in enumerate(schematic): 
+        numbers = num_regex.finditer(row)
+
+
+    
+
+def day4(example=False, reload=False):
+    day = 4 if not example else """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"""
+    puzzle = get_input(day, "\n", None, reload)
+    v = 0
+    cards = {}
+    for l in puzzle:
+        card, val = l.split(":")
+        win, have = val.split("|")
+        w_set = set()
+        h_set = set()
+        for w in win.split():
+            w_set.add(int(w))
+        for h in have.split():
+            h_set.add(int(h))
+        m = len(w_set.intersection(h_set))
+        s = 0
+        if m:
+            s = 1
+            for i in range(m-1):
+                s=s*2
+            v += s
+        print(w_set, h_set, s)
+        cards[int(card.split()[1])] = (m, s)
+    print(v)
+    print(cards)
+    v=0
+    card_list = list(cards.keys())
+    final_list = []
+    while card_list:
+        c = card_list.pop()
+        final_list.append(c)
+        if cards[c][0]:
+            for i in range(1,cards[c][0]+1):
+                card_list.append(c+i)
+    #print(card_list)
+    #print(final_list)
+    print(len(final_list))
 
 class Viz(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
