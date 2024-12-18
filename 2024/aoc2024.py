c562f71c09af0c2958a270d953b3c5e527929663
Tim Burbach <timburbach@gmail.com>
2024-12-18 01:35:36 -0600

    notes

--- 2024/aoc2024.py
+++ 2024/aoc2024.py
@@ -2122,12 +2122,11 @@ def day17(example=False, override=False, start=0,nmax=8):
     # return c
     # print(p2)
     # print(",".join(map(str, c.output)))
-
+    # Made it to 24500000000 wihtout finding the answer to 17p2.
 
 
 def day18(example=False, override=False):
     """Day 18."""
-    """Day 17."""
     day: int | str
     day = ("5,4\n4,2\n4,5\n3,0\n2,1\n6,3\n2,4\n1,5\n0,6\n3,3\n2,6\n5,1\n1,2\n5,5\n2,5\n"
            "6,5\n1,4\n0,4\n6,4\n1,1\n6,1\n1,0\n0,5\n1,6\n2,0")
